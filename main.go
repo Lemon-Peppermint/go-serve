ecb6d5717567b1a6a95ee5261fee94f17e76efc5
Lemon-Peppermint <141275572+Lemon-Peppermint@users.noreply.github.com>
2024-08-13 00:35:59 +0300

    Standard boilerplate + Initial static server

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "log"
-  "net"
-  "net/http"
-  "database/sql"
+	//"database/sql"
+	//"fmt"
+	"log"
+	//"net"
+	"net/http"
+	//"os"
 )
 
 func main() {
-  fmt.Println("Hello serve")
+	logger := log.Default()
+	logger.SetFlags(3)
+
+	log.Println("Hello serve")
+
+	log.Fatal(http.ListenAndServe("localhost:8000", http.FileServer(http.Dir("./static"))))
 }
